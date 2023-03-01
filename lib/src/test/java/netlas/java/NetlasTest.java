6eb9bdbe05de81ea72ccb8d43a006c89ef6800ed
Michael <michael2ga3@gmail.com>
2023-03-01 19:37:09 +0300

    Add test for query

--- lib/src/test/java/netlas/java/NetlasTest.java
+++ lib/src/test/java/netlas/java/NetlasTest.java
@@ -1,31 +1,61 @@
 package netlas.java;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class NetlasTest {
 
-  @Test
-  void testSearch() throws APIException {
-    String apiKey = System.getenv("API_KEY");
-    assertNotNull(apiKey);
-    assertNotEquals("", apiKey);
+    private Netlas netlas;
+    final private String API_KEY = System.getenv("API_KEY");
 
-    Netlas netlas = new Netlas(apiKey);
+    @BeforeEach
+    void setUp() {
+        assertNotNull(API_KEY);
+        assertNotEquals(0, API_KEY.length());
+        netlas = new Netlas(API_KEY);
+    }
 
-    String query = "https://google.com";
-    String datatype = "response";
-    int page = 0;
-    String indices = "";
-    String fields = "";
-    boolean excludeFields = false;
+    @Test
+    void testGetApiKey() {
+        assertEquals(API_KEY, netlas.getApiKey());
+    }
 
-    String response = netlas.search(query, datatype, page, indices, fields, excludeFields);
+    @Test
+    void testGetApiBase() {
+        assertEquals("https://app.netlas.io", netlas.getApiBase());
+    }
 
-    assertTrue(response.length() > 0);
-  }
+    @Test
+    void testIsDebug() {
+        assertFalse(netlas.isDebug());
+    }
+
+    @Test
+    void testGetConnectionTimeout() {
+        assertEquals(120, netlas.getConnectionTimeout());
+    }
+
+    @Test
+    void testGetClient() {
+        assertNotNull(netlas.getClient());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "response, port:7001",
+            "cert, certificate.subject.country:US",
+            "domain, mx:mail.google.com"
+    })
+    void testSearch(String dataType, String query) {
+        assertDoesNotThrow(() -> netlas.search(query, dataType, 0, null, null, false));
+    }
 
 }
